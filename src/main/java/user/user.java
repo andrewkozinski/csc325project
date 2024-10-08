e04521820d12200ca178c05d8c37830c00b86865
andrewkozinski <andrewkozinski76@gmail.com>
2024-10-08 10:39:11 -0400

    Added abstract methods to the abstract user class

--- src/main/java/user/user.java
+++ src/main/java/user/user.java
@@ -1,7 +1,26 @@
 package user;
 
+import java.util.Date;
+
 public abstract class user {
 
+    public abstract String getUserName();
 
+    public abstract void setUserName(String userName);
 
+    public abstract String getUserId();
+
+    public abstract void setUserId(String userId);
+
+    public abstract String getFirstName();
+
+    public abstract void setFirstName(String firstName);
+
+    public abstract String getLastName();
+
+    public abstract void setLastName(String lastName);
+
+    public abstract Date getBirthDate();
+
+    public abstract void setBirthDate(Date birthDate);
 }
