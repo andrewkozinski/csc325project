a316b9d0153b6744e586cc5a5621a18f435fd389
andrewkozinski <andrewkozinski76@gmail.com>
2024-10-10 22:55:59 -0400

    Added constructors to student, professor and admin

--- src/main/java/user/Admin.java
+++ src/main/java/user/Admin.java
@@ -6,8 +6,28 @@ package user;
  * Including but not limited to: userid, username, etc.
  * @author Andrew Kozinski
  */
-public class Admin {
+public class Admin extends User{
 
+    //Default constructor
+    /**
+     * Default constructor, just sets variable values to default (null)
+     */
+    public Admin() {
+        super();
+    }
 
+    //Parameterized constructor
+    public Admin(String username, String password, String firstName, String lastName, String userId) {
+        super(username, password, firstName, lastName, userId);
+    }
+
+    /**
+     * Returns a string containing information about the user
+     * @return String containing user information
+     */
+    @Override
+    public String userInfo() {
+        return "";
+    }
 
 }
