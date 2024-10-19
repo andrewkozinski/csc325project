da794191bb9ffc5309c4b51f8eba344e5cb8cd77
Yash <144078780+Y4shh@users.noreply.github.com>
2024-10-19 18:40:39 -0400

    Updated pom.xml & module-info to include Firebase Admin API and Apache Commons for password hashing.
    Added Firebase API credentials into resources/ directory and added to .gitignore.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module org.group3.csc325project {
     requires javafx.controls;
     requires javafx.fxml;
+    requires firebase.admin;
+    requires com.google.auth.oauth2;
+    requires org.apache.commons.codec;
 
 
     opens org.group3.csc325project to javafx.fxml;
