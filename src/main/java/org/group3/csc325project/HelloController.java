7ba81d09f3695015d74e2fba081d44b7d51294e3
andrewkozinski <andrewkozinski76@gmail.com>
2024-10-19 17:52:32 -0400

    Removed try/catch in HelloController

--- src/main/java/org/group3/csc325project/HelloController.java
+++ src/main/java/org/group3/csc325project/HelloController.java
@@ -23,33 +23,21 @@ public class HelloController {
      * On call, switches scene to student.fxml
      */
     public void switchToStudent() {
-        try {
-            HelloApplication.setRoot("student");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        HelloApplication.setRoot("student");
     }
 
     /**
      * On call, switches scene to professor.fxml
      */
     public void switchToProfessor() {
-        try {
-            HelloApplication.setRoot("professor");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        HelloApplication.setRoot("professor");
     }
 
     /**
      * On call, switches scene to admin.fxml
      */
     public void switchToAdmin() {
-        try {
-            HelloApplication.setRoot("admin");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        HelloApplication.setRoot("admin");
     }
 
 }
