9ffb54746b2d6d081cc1907f4ff31de711b1a5e9
andrewkozinski <andrewkozinski76@gmail.com>
2024-11-14 21:33:14 -0500

    Updated ProfessorCourses.java to be named ProfessorCoursesController.java (to keep a consistent naming scheme)

--- src/main/java/org/group3/csc325project/ProfessorCourses.java
+++
@@ -1,4 +0,0 @@
-package org.group3.csc325project;
-
-public class ProfessorCourses {
-}
---
+++ src/main/java/org/group3/csc325project/ProfessorCoursesController.java
@@ -0,0 +1,12 @@
+package org.group3.csc325project;
+
+/**
+ * ProfessorCoursesController
+ */
+public class ProfessorCoursesController {
+
+    public void initialize() {
+
+    }
+
+}
