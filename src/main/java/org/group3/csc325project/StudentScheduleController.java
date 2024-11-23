84f7ce1954cff7c1e713aafea7d6d9f60ddd5663
andrewkozinski <andrewkozinski76@gmail.com>
2024-11-22 20:44:42 -0500

    Added handleDropCourseButton() and handleGoBackButton() methods

--- src/main/java/org/group3/csc325project/StudentScheduleController.java
+++ src/main/java/org/group3/csc325project/StudentScheduleController.java
@@ -1,9 +1,15 @@
 package org.group3.csc325project;
 
+import javafx.scene.input.MouseEvent;
+
+import static org.group3.csc325project.RegistrationApp.setRoot;
+
 /**
  * Controller studentschedule.fxml. Handles the logic behind allowing a student to view what courses they're registered for
  */
 public class StudentScheduleController {
+
+
 
     /**
      * Runs when page is loaded, sets up columns and reads Firebase for what courses a student is registered for.
@@ -12,6 +18,21 @@ public class StudentScheduleController {
 
     }
 
+    /**
+     * Method that handles dropping a selected course
+     */
+    public void handleDropCourseButton() {
+        System.out.println("Drop course button pressed");
+    }
 
+
+    /**
+     * Method that switches scene to student.fxml
+     */
+    public void handleGoBackButton() {
+        setRoot("student");
+    }
+
+    public void handleCoursesTableViewMouseClick(MouseEvent event) {}
 
 }
