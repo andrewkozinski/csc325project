17d6821d722dfab26eb50cdc9d04899ec91cd9dc
andrewkozinski <andrewkozinski76@gmail.com>
2024-11-22 19:26:02 -0500

    Set up initialize and handleClick methods. All necessary member variables now implemented.

--- src/main/java/org/group3/csc325project/StudentEnrollController.java
+++ src/main/java/org/group3/csc325project/StudentEnrollController.java
@@ -1,8 +1,127 @@
 package org.group3.csc325project;
 
+import course.Course;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import user.Student;
+
 /**
  * Controller for studentenroll.fxml. Handles the logic behind allowing a student to view and enroll in courses.
  */
 public class StudentEnrollController {
+    //Note: Much of the following code is largely the same as CoursesController
+    //I've kept variable names largely the same across both StudentEnrollController and CoursesController
+
+    //Just took this from CoursesController, feel free to change as you'd like Yash
+    private static final Logger logger = LoggerFactory.getLogger(CoursesController.class);
+
+    //TableView where course information is stored
+    @FXML
+    private TableView<Course> coursesTable;
+    //Column where the course CRN is displayed
+    @FXML
+    private TableColumn<Course, String> columnCrn;
+    //Column where the course code is displayed
+    @FXML
+    private TableColumn<Course, String> columnCourseCode;
+    //Column where the course name is displayed
+    @FXML
+    private TableColumn<Course, String> columnCourseName;
+    //Column where the course instructor is displayed
+    @FXML
+    private TableColumn<Course, String> columnInstructor;
+    //Column where the number of credits a course is worth is displayed
+    @FXML
+    private TableColumn<Course, String> columnCredits;
+    //Column where the course meeting time is displayed
+    @FXML
+    private TableColumn<Course, String> columnCourseSchedule;
+    //Column where the days a course meets is displayed
+    @FXML
+    private TableColumn<Course, String> columnCourseDays;
+    //Column where the room a course takes place is displayed
+    @FXML
+    private TableColumn<Course, String> columnCourseLocation;
+    //Column where the course capacity is displayed
+    @FXML
+    private TableColumn<Course, String> columnCourseCapacity;
+    @FXML
+    private TableColumn<Course, String> columnCourseWaitlist;
+    //TableView where Students enrolled in the selected course are displayed
+    @FXML
+    private TableView<Student> studentsTable;
+    //Column where the student name is displayed
+    @FXML
+    private TableColumn<Student, String> columnStudentName;
+    //Column where the student ID is displayed
+    @FXML
+    private TableColumn<Student, String> columnStudentID;
+    //Currently selected course from the TableView
+    private Course selectedCourse;
+
+    /**
+     * Runs when page is loaded. Each column in the TableView is associated with a variable in the Course class
+     */
+    public void initialize() {
+        columnCrn.setCellValueFactory(new PropertyValueFactory<>("courseCRN"));
+        columnCourseCode.setCellValueFactory(new PropertyValueFactory<>("courseCode"));
+        columnCourseName.setCellValueFactory(new PropertyValueFactory<>("courseName"));
+        columnInstructor.setCellValueFactory(cellData -> cellData.getValue().professorNameProperty());
+        columnCredits.setCellValueFactory(cellData -> {
+            String returnString = String.format("%d", cellData.getValue().getCredits());
+            return new SimpleStringProperty(returnString);
+        });
+        columnCourseSchedule.setCellValueFactory(new PropertyValueFactory<>("courseTime"));
+        columnCourseDays.setCellValueFactory(new PropertyValueFactory<>("courseDays"));
+        columnCourseLocation.setCellValueFactory(new PropertyValueFactory<>("courseLocation"));
+        columnCourseCapacity.setCellValueFactory(cellData -> {
+            int capacity = cellData.getValue().getCapacity();
+            int enrolled = cellData.getValue().getCurrentEnrolledCount();
+            String returnString = String.format("%d/%d", enrolled, capacity);
+            return new SimpleStringProperty(returnString);
+        });
+        columnCourseWaitlist.setCellValueFactory(cellData -> {
+            int currentWaitlistCount = cellData.getValue().getCurrentWaitlistCount();
+            return new SimpleStringProperty(String.valueOf(currentWaitlistCount));
+        });
+
+        //Test add
+        Course test = new Course();
+        test.setCourseName("8k Game Programming");
+        coursesTable.getItems().add(test);
+
+        // Reads Course collection in the Firestore database and adds those courses to the TableView
+        handleReadFirebase();
+
+    }
+
+    /**
+     * Helper method which reads the Firestore course collection and adds those courses to the TableView
+     */
+    private void handleReadFirebase() {
+
+    }
+
+    /**
+     * Handles the user selecting an item in the TableView
+     * Updates the selectedItem variable
+     * @param event mouse click event
+     */
+    public void handleCoursesTableViewMouseClick(MouseEvent event) {
+        ObservableList<Course> courses = coursesTable.getItems();
+        int selectedIndex = coursesTable.getSelectionModel().getSelectedIndex();
+        if (selectedIndex >= 0 && selectedIndex < courses.size()) {
+            //Sets selected course to what was clicked
+            selectedCourse = courses.get(selectedIndex);
+            logger.info("Selected course: {} - {}", selectedCourse.getCourseName(), selectedCourse.getProfessor());
+        }
+    }
 
 }
