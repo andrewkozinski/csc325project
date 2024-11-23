135f24d9f359241b8ba089821ede068fad904859
andrewkozinski <andrewkozinski76@gmail.com>
2024-11-22 20:33:48 -0500

    Created studentschedule.fxml and StudentScheduleController.java

---
+++ src/main/java/org/group3/csc325project/StudentScheduleController.java
@@ -0,0 +1,17 @@
+package org.group3.csc325project;
+
+/**
+ * Controller studentschedule.fxml. Handles the logic behind allowing a student to view what courses they're registered for
+ */
+public class StudentScheduleController {
+
+    /**
+     * Runs when page is loaded, sets up columns and reads Firebase for what courses a student is registered for.
+     */
+    public void initialize() {
+
+    }
+
+
+
+}
