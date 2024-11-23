eab74db31af99210373df9a135824fce52cb006e
andrewkozinski <andrewkozinski76@gmail.com>
2024-11-22 19:03:17 -0500

    Created studentenroll.fxml and StudentEnrollController.java. This will be a page to handle a student registering for a course

---
+++ src/main/java/org/group3/csc325project/StudentEnrollController.java
@@ -0,0 +1,8 @@
+package org.group3.csc325project;
+
+/**
+ * Controller for studentenroll.fxml. Handles the logic behind allowing a student to view and enroll in courses.
+ */
+public class StudentEnrollController {
+
+}
