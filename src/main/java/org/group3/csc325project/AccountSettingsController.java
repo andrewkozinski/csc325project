ef280812e1257512198a60fe39641410b455b5fa
andrewkozinski <andrewkozinski76@gmail.com>
2024-12-02 13:13:36 -0500

    Added getCurrentUserFromFirebase method

--- src/main/java/org/group3/csc325project/AccountSettingsController.java
+++ src/main/java/org/group3/csc325project/AccountSettingsController.java
@@ -1,11 +1,17 @@
 package org.group3.csc325project;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.*;
+import com.google.firebase.cloud.FirestoreClient;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import user.Admin;
 import user.Professor;
 import user.Student;
 import user.User;
+
+import java.util.concurrent.ExecutionException;
 
 public class AccountSettingsController {
     @FXML
@@ -27,9 +33,70 @@ public class AccountSettingsController {
     /**
      * When called, allows a selected user to be edited
      */
-    String Username = SessionManager.getLoggedInUsername();
-    String Role = SessionManager.getLoggedInUserRole();
+    String username = SessionManager.getLoggedInUsername();
+    String role = SessionManager.getLoggedInUserRole();
     boolean twoFactor = SessionManager.getTwoFAEnabled();
+
+    /**
+     * Runs when the AccountSettings page is loaded
+     */
+    public void initialize() {
+        selectedUser = getCurrentUserFromFirebase();
+    }
+
+    /**
+     * Gets user from firebase and stores information into a User instance
+     * @return A new User object instance
+     */
+    public User getCurrentUserFromFirebase() {
+        Firestore db = FirestoreClient.getFirestore();
+        CollectionReference collection = db.collection(username);
+
+        try {
+            ApiFuture<QuerySnapshot> future = collection.whereEqualTo("Username", username).get();
+            QuerySnapshot querySnapshot = future.get();
+            for (QueryDocumentSnapshot document : querySnapshot) {
+                if (role.equals("Student")) {
+                    Student student = document.toObject(Student.class);
+                    student.setUserId(username);
+                    student.setUsername(username);
+                    student.setMajor(document.getString("Major"));
+                    student.setClassification(document.getString("Classification"));
+                    student.setAge(document.getString("Age"));
+                    student.setFirstName(document.getString("FirstName"));
+                    student.setLastName(document.getString("LastName"));
+                    student.setEmail(document.getString("Email"));
+                    return student;
+                } else if (role.equals("Professor")) {
+                    Professor professor = document.toObject(Professor.class);
+                    professor.setUserId(username);
+                    professor.setUsername(username);
+                    professor.setDepartment(document.getString("Department"));
+                    professor.setAge(document.getString("Age"));
+                    professor.setFirstName(document.getString("FirstName"));
+                    professor.setLastName(document.getString("LastName"));
+                    professor.setEmail(document.getString("Email"));
+                    return professor;
+                }
+                else if(role.equals("Admin")) {
+                    Admin admin = document.toObject(Admin.class);
+                    admin.setUserId(username);
+                    admin.setUsername(username);
+                    admin.setAge(document.getString("Age"));
+                    admin.setFirstName(document.getString("FirstName"));
+                    admin.setLastName(document.getString("LastName"));
+                    admin.setEmail(document.getString("Email"));
+                    return admin;
+                }
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
+        return null;
+
+    }
+
     /*
     public void handleEditUser() {
         AccountsController ac = new AccountsController();
