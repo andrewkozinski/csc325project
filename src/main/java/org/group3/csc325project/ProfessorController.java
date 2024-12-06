ce5a09ef9ece6898235ffe8a4f6ef3151edf1b40
andrewkozinski <andrewkozinski76@gmail.com>
2024-12-06 16:13:45 -0500

    Updated openAccountButton method name

--- src/main/java/org/group3/csc325project/ProfessorController.java
+++ src/main/java/org/group3/csc325project/ProfessorController.java
@@ -72,8 +72,7 @@ public class ProfessorController {
     /**
      * Opens account dropdown menu
      */
-    public void openAccount_button(MouseEvent event) {
+    public void openAccountButton(MouseEvent event) {
         RegistrationApp.openAccount_button(event);
-
     }
 }
